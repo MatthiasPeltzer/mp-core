ed029485ffb15b8cc02c04b51fb307cc1199e280
MADPC\matth <mail@mpeltzer.de>
2025-11-11 08:16:27 +0100

    feat: composer only

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,27 @@
 <?php
 
+/**
+ * This extension requires Composer and cannot be installed via TER/Extension Manager.
+ *
+ * Installation:
+ * composer require mpc/mp-core
+ *
+ * Dependencies (b13/container, friendsoftypo3/content-blocks) are only available
+ * through Composer/Packagist and are defined in composer.json.
+ */
+
 $_EXTKEY = "mp_core";
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'MPC Distribution Package',
-    'description' => 'Basic TYPO3 Templateset',
+    'description' => 'Basic TYPO3 Templateset (Composer-only)',
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0 - 14.4.99',
-            'b13/container' => '3.1',
-            'friendsoftypo3/content-blocks' => '1.3',
+            'typo3' => '13.4.0-14.4.99',
         ],
         'conflicts' => [],
-        'suggests' => [
-            'georgringer/news' => '13.0',
-            'georgringer/numbered-pagination' => '2.1',
-        ],
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'Mpc\\MpCore\\' => 'Classes'
-        ],
+        'suggests' => [],
     ],
     'state' => 'stable',
     'uploadfolder' => 0,
