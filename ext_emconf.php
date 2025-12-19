2447b8851c7e6f0853cb00a0cc232a43553ef5c4
Matthias <mail@mpeltzer.de>
2025-12-19 09:12:31 +0100

    fix: v1.0.26

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,24 @@
 <?php
 
-/**
- * This extension requires Composer and cannot be installed via TER/Extension Manager.
- *
- * Installation:
- * composer require mpc/mp-core
- *
- * Dependencies (b13/container, friendsoftypo3/content-blocks) are only available
- * through Composer/Packagist and are defined in composer.json.
- */
-
-$_EXTKEY = "mp_core";
-
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['mp_core'] = [
     'title' => 'MPC Distribution Package',
-    'description' => 'Basic TYPO3 Templateset (Composer-only)',
+    'description' => 'Core TYPO3 CMS extension providing base templates, styling, container, content blocks and backend configurations',
     'category' => 'templates',
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-14.3.99',
+            'rte_ckeditor' => '13.4.0-14.3.99',
+            'seo' => '13.4.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
     ],
+    'autoload' => [
+        'psr-4' => [
+            'Mpc\\MpCore\\' => 'Classes',
+        ],
+    ],
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
     'author' => 'Matthias Peltzer',
     'author_email' => 'mail@mpeltzer.de',
     'author_company' => 'private',
