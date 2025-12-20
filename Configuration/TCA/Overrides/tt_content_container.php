4327ded3ec5c84bd316d7dd714fa9f713f806f9f
Matthias <mail@mpeltzer.de>
2025-12-20 07:56:51 +0100

    feat: grid - bootstrap 5.3 responsive grid controls (breakpoint, widths, offsets, gutter)

--- Configuration/TCA/Overrides/tt_content_container.php
+++ Configuration/TCA/Overrides/tt_content_container.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 use B13\Container\Tca\ContainerConfiguration;
 use B13\Container\Tca\Registry;
+use Mpc\MpCore\UserFunc\ColorPickerValueItems;
+use TYPO3\CMS\Core\Resource\FileType;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') or die('Access denied.');
@@ -29,6 +32,262 @@ defined('TYPO3') or die('Access denied.');
         ->setIcon('tx_container')
         ->setSaveAndCloseInNewContentElementWizard(false)
     );
+
+    // Container background / wrapper options (only for ce_container)
+    $containerFields = [
+        'grid_bgcolor' => [
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.bgcolor',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.bgcolor.description',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [],
+                'itemsProcFunc' => ColorPickerValueItems::class . '->getItems',
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ],
+        ],
+        'grid_icon_switch' => [
+            'exclude' => '0',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.grid_icon_switch',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.grid_icon_switch.description',
+            'onChange' => 'reload',
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.grid_icon_switch',
+                        'labelChecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+                        'labelUnchecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disabled',
+                    ],
+                ],
+                'default' => '0',
+            ],
+        ],
+        'grid_icon' => [
+            'exclude' => '1',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.icon',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.icon.description',
+            'displayCond' => 'FIELD:grid_icon_switch:=:1',
+            'config' => [
+                'type' => 'file',
+                'allowed' => 'png,jpg,jpeg,gif,svg,webp',
+                'maxitems' => 1,
+                'appearance' => [
+                    'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference',
+                    'showPossibleLocalizationRecords' => true,
+                ],
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+                'overrideChildTca' => [
+                    'columns' => [
+                        'description' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'link' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'title' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'outline' => [
+                            'config' => [
+                                'renderType' => 'passthrough',
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'caption' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                    ],
+                    'types' => [
+                        '0' => [
+                            'showitem' => '
+                                --palette--;;imageoverlayPalette,
+                                --palette--;;filePalette',
+                        ],
+                        FileType::TEXT->value => [
+                            'showitem' => '
+                                --palette--;;imageoverlayPalette,
+                                --palette--;;filePalette',
+                        ],
+                        FileType::IMAGE->value => [
+                            'showitem' => '
+                                --palette--;;imageoverlayPalette,
+                                --palette--;;filePalette',
+                        ],
+                        FileType::AUDIO->value => [
+                            'showitem' => '
+                                --palette--;;audioOverlayPalette,
+                                --palette--;;filePalette',
+                        ],
+                        FileType::VIDEO->value => [
+                            'showitem' => '
+                                --palette--;;videoOverlayPalette,
+                                --palette--;;filePalette',
+                        ],
+                        FileType::APPLICATION->value => [
+                            'showitem' => '
+                                --palette--;;imageoverlayPalette,
+                                --palette--;;filePalette',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        'grid_light' => [
+            'exclude' => '0',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.light',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.light.description',
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.light',
+                        'labelChecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+                        'labelUnchecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disabled',
+                    ],
+                ],
+                'default' => '0',
+            ],
+        ],
+        'grid_parallax' => [
+            'exclude' => '0',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.parallax',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.parallax.description',
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.parallax',
+                        'labelChecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+                        'labelUnchecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disabled',
+                    ],
+                ],
+                'default' => '0',
+            ],
+        ],
+        'grid_bgfullsize' => [
+            'exclude' => '0',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.bgfullsize',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.bgfullsize.description',
+            'displayCond' => 'FIELD:grid_icon_switch:=:0',
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.bgfullsize',
+                        'labelChecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+                        'labelUnchecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disabled',
+                    ],
+                ],
+                'default' => '0',
+            ],
+        ],
+        'grid_container' => [
+            'exclude' => '0',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.container',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.container.description',
+            'displayCond' => 'FIELD:grid_icon_switch:=:0',
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bg.container',
+                        'labelChecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+                        'labelUnchecked' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disabled',
+                    ],
+                ],
+                'default' => '0',
+            ],
+        ],
+        'grid_bgimage' => [
+            'exclude' => '1',
+            'label' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bgimage.container',
+            'description' => 'LLL:EXT:mp_core/Resources/Private/Language/locallang_db.xlf:grid.bgimage.container.description',
+            'config' => [
+                'type' => 'file',
+                'allowed' => 'png,jpg,jpeg,gif,svg,webp',
+                'maxitems' => '1',
+                'appearance' => [
+                    'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:images.addFileReference',
+                    'showPossibleLocalizationRecords' => true,
+                ],
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+                'overrideChildTca' => [
+                    'columns' => [
+                        'description' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'link' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'title' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'outline' => [
+                            'config' => [
+                                'renderType' => 'passthrough',
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                        'caption' => [
+                            'config' => [
+                                'type' => 'passthrough',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
+
+    $containerPalettes = [
+        'grid_bg' => [
+            'showitem' => 'grid_bgcolor,grid_light,grid_icon_switch,--linebreak--,grid_icon,--linebreak--,grid_bgimage,--linebreak--,grid_parallax',
+            'canNotCollapse' => 1,
+        ],
+        'grid_container_pallet' => [
+            'showitem' => 'grid_bgfullsize,grid_container',
+            'canNotCollapse' => 1,
+        ],
+    ];
+
+    $GLOBALS['TCA']['tt_content']['palettes'] += $containerPalettes;
+    ExtensionManagementUtility::addTCAcolumns('tt_content', $containerFields);
 
     // override default settings
     $GLOBALS['TCA']['tt_content']['types']['ce_container']['showitem'] = '
