0705a159b18520db43615a8f76d3796371addffc
Matthias <mail@mpeltzer.de>
2026-01-17 11:27:28 +0100

    feat: ckeditor - add dl/dt/dd support and enable advanced list properties

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -6,6 +6,7 @@ return [
         'backend.form',
     ],
     'imports' => [
-
+        // CKEditor 5 Definition List Plugin
+        '@mpc/mp-core/ckeditor/definition-list.js' => 'EXT:mp_core/Resources/Public/JavaScripts/ckeditor/definition-list.js',
     ],
 ];
