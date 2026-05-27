bd84cab796012a2d97ead7459072b7e9d750dac3
Matthias <mail@mpeltzer.de>
2026-05-27 21:25:58 +0200

    release: v1.1.23

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,5 +26,5 @@ $EM_CONF['mp_core'] = [
     'author' => 'Matthias Peltzer',
     'author_email' => 'mail@mpcore.de',
     'author_company' => 'private',
-    'version' => '1.1.22',
+    'version' => '1.1.23',
 ];
